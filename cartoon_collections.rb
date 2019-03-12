bde57613f2a674bdf2d0023a729a737a4de824b6
Jesse Reeves <reeveswriting@gmail.com>
2019-03-12 15:48:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element|
-    element.capitalize << !
+    element.capitalize << "!"
 end
 
 def long_planeteer_calls# code an argument here
